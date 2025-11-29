e727e41ddd39f1c58f94aa34d3ec3db99879c717
Cassandra <cassandralonjonsalis@gmail.com>
2025-11-29 16:10:33 +0100

    commit 13

---
+++ test.php
@@ -0,0 +1,6 @@
+<?php include 'header.php'; ?>
+
+<main>
+    <h2>Page d'accueil</h2>
+    <p>Contenu principal ici.</p>
+</main>
---
+++ test_header.php
@@ -0,0 +1,15 @@
+    <header>
+        <div class="container">
+            <img src="Logo AEM.png" alt="Logo de Armorique En avant Morbihan" class="Logo">
+            <a href="accueil.html" class="headband">Accueil</a>
+            <a href="equipe.html" class="headband">Equipe</a>
+            <a href="boutique.html" class="headband">Boutique</a>
+            <a href="#" class="headband">Panier (<span id="cart-count">0</span>)</a>
+
+            <div class="header-toggle">
+                <!-- Theme toggle -->
+                <input type="checkbox" id="darkmode-toggle">
+                <label for="darkmode-toggle" id="darkmod-label"></label>
+            </div>
+        </div>
+    </header>
